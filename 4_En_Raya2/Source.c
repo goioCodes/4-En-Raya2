c90de7aff4a4f4c48623b8114e50d78e47890cbf
blastoise-pro <32550964+blastoise-pro@users.noreply.github.com>
2022-09-27 23:32:38 +0200

    Añadidos

--- 4_En_Raya2/Source.c
+++ 4_En_Raya2/Source.c
@@ -1,16 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main()
 {
+	// Inicialització de GLFW
 	glfwInit();
+	// Configuració de GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Creació de la finestra
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Epic window", NULL, NULL);
 	if (window == NULL)
 	{
-		printf("Can't create window");
+		printf("No s'ha pogut crear la finestra");
 		glfwTerminate();
+		return -1;
 	}
+	glfwMakeContextCurrent(window);
+
+	// Carregar totes les funcions de OpenGL a traves de GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("No s'ha pogut inicialitzar GLAD");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
