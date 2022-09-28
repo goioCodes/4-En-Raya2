3e079315c905e3e7ea50bbb0710e1557b101f856
blastoise-pro <32550964+blastoise-pro@users.noreply.github.com>
2022-09-28 19:08:17 +0200

    2 triangulos

--- 4_En_Raya2/Source.c
+++ 4_En_Raya2/Source.c
@@ -7,6 +7,7 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 const char* vertexShaderSource = "#version 460 core\n"
 	"layout(location = 0) in vec3 aPos;\n"
@@ -16,12 +17,20 @@ const char* vertexShaderSource = "#version 460 core\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\0";
 
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 460 core\n"
 	"out vec4 FragColor; \n"
 	"\n"
 	"void main()\n"
 	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"	FragColor = vec4(1.0f, 0.8f, 0.8f, 1.0f);\n"
+	"}";
+
+const char* fragmentShaderSource2 = "#version 460 core\n"
+	"out vec4 FragColor; \n"
+	"\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f);\n"
 	"}";
 
 int main()
@@ -52,28 +61,54 @@ int main()
 
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
 
-	glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
 
-	// Triangle
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	// Triangles
+	float vertices1[] = {
+	-1.0f, -0.5f, 0.0f,  // left
+	 0.0f,  0.0f, 0.0f,  // top
+	-0.5f, -1.0f, 0.0f   // right
+	};  // right
+	// segon triangle
+	float vertices2[] = {
+	 0.0f,  0.0f, 0.0f,
+	 0.5f,  1.0f, 0.0f,  // bottom left
+	 1.0f,  0.5f, 0.0f   // top left 
 	};
-	unsigned int VBO;
-	glGenBuffers(1, &VBO); // Creem un vertex buffer a la GPU
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Vinculem el buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copiem les dades al buffer
-
+	float* vertices[] = { vertices1, vertices2 };
+	printf("sizeof(vertices) %zi\n", sizeof(vertices));
+	printf("sizeof(vertices1) %zi\n", sizeof(vertices[0]));
+	/*
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};*/
 	// Creem el Vertex Array Object
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	// El VAO guardarà tota la configuració de vertex attributes que farem a continuació
+	unsigned int VAOs[2];
+	glGenVertexArrays(2, VAOs);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // Especifiquem com les dades del vertex buffer han de ser interpretades pel shader
-	glEnableVertexAttribArray(0); // Necessari, no activat per defecte
+	// El VAO guardarà tota la configuració de vertex attributes que farem a continuació, i el EBO que vinculem
+	unsigned int VBOs[2];
+	glGenBuffers(2, VBOs); // Creem un vertex buffer a la GPU
+
+	for (int i = 0; i < 2; i++)
+	{
+		glBindVertexArray(VAOs[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, VBOs[i]); // Vinculem el vertex buffer
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), vertices[i], GL_STATIC_DRAW); // Copiem les dades al buffer
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // Especifiquem com les dades del vertex buffer han de ser interpretades per la GPU
+		glEnableVertexAttribArray(0); // Necessari, no activat per defecte
+	}
+
+	/*
+	// Creem el Element Buffer Array
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	*/
+	
 
 	// VERTEX SHADER
 	unsigned int vertexShader;
@@ -100,6 +135,18 @@ int main()
 		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
 	}
 
+	//FRAGMENT SHADER 2
+	unsigned int fragmentShader2;
+	fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER); // Creem el fragment shader
+	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+	glCompileShader(fragmentShader2);
+	glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+	}
+
 	// Shader program
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
@@ -112,19 +159,37 @@ int main()
 		printf("ERROR::SHADER::PROGRAM::LINK_FAILED\n%s\n", infoLog);
 	}
 
+	// Shader program 2
+	unsigned int shaderProgram2;
+	shaderProgram2 = glCreateProgram();
+	glAttachShader(shaderProgram2, vertexShader);
+	glAttachShader(shaderProgram2, fragmentShader2);
+	glLinkProgram(shaderProgram2);
+	glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+		printf("ERROR::SHADER::PROGRAM::LINK_FAILED\n%s\n", infoLog);
+	}
+
 	// Neteja
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader2);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
 		processInput(window);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(VAOs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glUseProgram(shaderProgram2);
+		glBindVertexArray(VAOs[1]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
@@ -132,6 +197,16 @@ int main()
 
 	glfwTerminate();
 	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
+	{
+		int i;
+		glGetIntegerv(GL_POLYGON_MODE, &i);
+		glPolygonMode(GL_FRONT_AND_BACK, i == GL_FILL ? GL_LINE : GL_FILL);
+	}
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
