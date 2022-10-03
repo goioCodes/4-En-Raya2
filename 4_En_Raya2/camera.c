ce33c0ee56e72a8ba6eb6d29468ecdee6cf9a8f0
blastoise-pro <32550964+blastoise-pro@users.noreply.github.com>
2022-10-03 19:08:46 +0200

    processKeyboard

--- 4_En_Raya2/camera.c
+++ 4_En_Raya2/camera.c
@@ -2,13 +2,21 @@
 
 const float YAW = -90.f;
 const float PITCH = 0.f;
+const float SENSITIVITY = 0.1f;
+const float MOVEMENTSPEED = 2.5f;
 const vec3 FRONT = { 0.f, 0.f, -1.f };
+
+typedef enum camera_movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+} Camera_Movement;
 
 typedef struct camera {
     vec3 position;
     vec3 front;
     vec3 up;
-    vec3 front;
     vec3 right;
     vec3 worldUp;
 
@@ -18,6 +26,8 @@ typedef struct camera {
     float movementSpeed;
     float mouseSensitivity;
 } Camera;
+
+void updateCamVectors(Camera* cam);
 
 void initializeCamera(Camera* cam, vec3 position, vec3 up)
 {
@@ -29,6 +39,8 @@ void initializeCamera(Camera* cam, vec3 position, vec3 up)
     }
     cam->yaw = YAW;
     cam->pitch = PITCH;
+    cam->movementSpeed = MOVEMENTSPEED;
+    cam->mouseSensitivity = SENSITIVITY;
     updateCamVectors(cam);
 }
 
@@ -37,26 +49,57 @@ void updateCamVectors(Camera* cam)
     cam->front[0] = cosf(glm_rad(cam->yaw)) * cosf(glm_rad(cam->pitch));
     cam->front[1] = sinf(glm_rad(cam->pitch));
     cam->front[2] = sinf(glm_rad(cam->yaw)) * cosf(glm_rad(cam->pitch));
+    glm_vec3_normalize(cam->front);
 
     glm_vec3_cross(cam->front, cam->worldUp, cam->right);
     glm_vec3_normalize(cam->right);
     glm_vec3_cross(cam->right, cam->front, cam->up);
+    glm_vec3_normalize(cam->up);
 
 }
 
-void processMouseEvent(Camera* cam, float xoffset, float yoffset)
+void processMouseMovement(Camera* cam, float xoffset, float yoffset)
 {   
+    xoffset *= cam->mouseSensitivity;
+    yoffset *= cam->mouseSensitivity;
+
     cam->yaw += xoffset;
     cam->pitch += yoffset;
 
-    if (cam->pitch >= 89.f)
+    if (cam->pitch > 89.f)
     {
         cam->pitch = 89.f;
     }
-    else if (cam->pitch <= -89.f)
+    if (cam->pitch < -89.f)
     {
         cam->pitch = -89.f;
     }
+
+    updateCamVectors(cam);
 }
 
-void processKeyboradEvent(Camera* cam, float xd);
+void processKeyborad(Camera* cam, Camera_Movement direction, float deltaTime)
+{
+    float velocity = cam->movementSpeed * deltaTime;
+    vec3 temp;
+    if (direction == FORWARD)
+    {
+        glm_vec3_scale(cam->front, velocity, temp);
+        glm_vec3_add(cam->position, temp, cam->position);
+    }
+    if (direction == BACKWARD)
+    {
+        glm_vec3_scale(cam->front, velocity, temp);
+        glm_vec3_sub(cam->position, temp, cam->position);
+    }
+    if (direction == LEFT)
+    {
+        glm_vec3_scale(cam->right, velocity, temp);
+        glm_vec3_sub(cam->position, temp, cam->position);
+    }
+    if (direction == RIGHT)
+    {
+        glm_vec3_scale(cam->right, velocity, temp);
+        glm_vec3_add(cam->position, temp, cam->position);
+    }
+}
